ed30da2eebb13f9834cbe093eb88b7420805b2ef
Randall Chacón <info@randallchacon.com>
2023-08-26 20:18:20 -0600

    Using reference and observer types

--- components/random-fox.tsx
+++ components/random-fox.tsx
@@ -1,12 +1,17 @@
 //import type { FunctionComponent, FC } from "react"
+import { useRef} from 'react' ;
+
 type Props = {
     image : string 
     alt : string
 };
 
 export const RandomFox = ({image, alt}: Props) : JSX.Element => { //best way to used it
-
-    return <img width={320} height="auto" className="rounded" src={image} alt={alt}/>;
+    const node = useRef<HTMLImageElement>(null); //set as null to avoid typescript issues
+    
+    return (
+        <img ref={node} width={320} height="auto" className="rounded" src={image} alt={alt}/>
+    );
 };
 
 // export const RandomFox = () => {
